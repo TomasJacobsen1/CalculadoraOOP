3909a44d2aff1734f325005f6992edc9c84a7807
tomas <tomasjacobsen@gmail.com>
2020-09-30 12:56:28 -0300

    actualizado

---
+++ GetOperador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadoraOOP
+{
+    class GetOp
+    {
+        public static IOperacion GetOperacion()
+        {
+            char op;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese un operador");
+                    op = char.Parse(Console.ReadLine());
+
+                    OPERADORES OP = (OPERADORES)op;
+
+                    switch (OP)
+                    {
+                        case OPERADORES.sum:
+                            return new Suma();
+                        case OPERADORES.rest:
+                            return new Resta();
+                        case OPERADORES.div:
+                            return new Division();
+                        case OPERADORES.mult:
+                            return new Multiplicacion();
+                        default:
+                            Console.WriteLine("El operador ingresado es incorrecto");
+                            break;
+
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("El valor ingresado incorrecto");
+                }
+            }
+        }
+    }
+    public enum OPERADORES { sum = '+', rest = '-', div = '/', mult = '*' }
+}
--- Program.cs
+++ Program.cs
@@ -9,18 +9,8 @@ namespace calculadoraOOP
 
             double num1;
             double num2;
-            char op;
-            Suma sumar = new Suma();
-            Resta restar = new Resta();
-            Division dividir = new Division();
-            Multiplicacion multiplicar = new Multiplicacion();
-
-            IOperacion[] aOperadores = new IOperacion[4];
-            aOperadores[0] = sumar;
-            aOperadores[1] = restar;
-            aOperadores[2] = dividir;
-            aOperadores[3] = multiplicar;
-
+            
+           
             while (true)
             {
                 try
@@ -34,26 +24,8 @@ namespace calculadoraOOP
                     Console.WriteLine("El valor ingresado incorrecto");
                 }
             }
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine("Ingrese un operador");
-                    op = char.Parse(Console.ReadLine());
-                    
-                    if (op != '+' && op != '-' && op != '*' && op != '/')
-                    {
-                        Console.WriteLine("El operador ingresado es desconocido");
-                        continue;
-                    }
-                    else { break; }
+            IOperacion ejecutar = GetOp.GetOperacion(); 
 
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("El valor ingresado incorrecto");
-                }
-            }
             while (true)
             {
                 try
@@ -67,52 +39,10 @@ namespace calculadoraOOP
                     Console.WriteLine("El valor ingresado incorrecto");
                 }
             }
-
-            OPERADORES OP = (OPERADORES)op;
-
-            switch (OP)
-            {
-                case OPERADORES.sum :
-                    Console.WriteLine(aOperadores[0].Ejecutar(num1, num2));
-                    break;
-                case OPERADORES.rest:
-                    Console.WriteLine(aOperadores[1].Ejecutar(num1, num2));
-                    break;
-                case OPERADORES.div:
-                    Console.WriteLine(aOperadores[2].Ejecutar(num1, num2));
-                    break;
-                case OPERADORES.mult:
-                    Console.WriteLine(aOperadores[3].Ejecutar(num1, num2));
-                    break;
-
-            }
-
-            /*if (op == '+')
-            {
-                Console.WriteLine(Sumar(num1, num2));
-            }
-            else if (op == '-')
-            {
-                Console.WriteLine(Restar(num1, num2));
-            }
-            else if (op == '*')
-            {
-                Console.WriteLine(Mulitplicar(num1, num2));
-            }
-            else if (op == '/')
-            {
-                if (num2 != 0)
-                {
-                    Console.WriteLine(Dividir(num1, num2));
-                }
-                else
-                {
-                    Console.WriteLine("Imposible Dividir por cero");
-                }
-            }*/
+            
+            Console.WriteLine(ejecutar.Ejecutar(num1 , num2));
         }
+
+
     }
-    public enum OPERADORES {sum='+' , rest='-' , div='/' , mult='*' }
-
-
 }
